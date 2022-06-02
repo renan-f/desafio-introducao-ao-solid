2bbdbaed967f62fa483abd7eadb20b4f7624f9e2
Renan Filgueiras Andrade <filgueiras.rfa@gmail.com>
2022-06-01 22:14:52 -0300

    Listagem de usuários

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,10 +3,17 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const user_id = String(request.headers.user_id);
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      
+      return response.json(users);
+    } catch (error) {
+      return response.status(400).send({ error: error.message });
+    }
   }
 }
 
